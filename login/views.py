90a930116916a271d39e4ec0b1afdf47308d6a44
yr <526230112@qq.com>
2020-06-25 15:05:35 +0800

    第二次修改

--- login/views.py
+++ login/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render,HttpResponse
 
 # Create your views here.
 def login(request):
-    print('登陆成功')
-    return HttpResponse('测试登录')
+    print('登陆成')
+    return HttpResponse('测试录')
